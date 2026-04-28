cedd0e6831b81d4899326345689446829ae92de6
JKrishnaD <jayak5063@gmail.com>
2026-04-28 14:36:02 +0530

    feat: export everything in index

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+// Types
+export {
+  Region,
+  type Attestation,
+  type RegionScore,
+  type NetworkHealth,
+  type Observer,
+  type Registry,
+  type AttestationSubmittedEvent,
+  type ObserverRegisteredEvent,
+  type ObserverDeregisteredEvent,
+  type ObserverSlashedEvent,
+  type ConfigUpdatedEvent,
+} from "./types.js";
+
+// PDA helpers and program ID
+export {
+  PROGRAM_ID,
+  getRegistryPDA,
+  getNetworkHealthPDA,
+  getObserverPDA,
+} from "./accounts.js";
+
+// Client
+export {
+  createS0narClient,
+  type S0narClient,
+  type S0narClientOptions,
+} from "./client.js";
+
+// Instruction params
+export type {
+  SubmitAttestationParams,
+  UpdateConfigParams,
+} from "./instructions.js";
+
+// Utility helpers
+export {
+  isStale,
+  isObserverStale,
+  isDegraded,
+  healthStatus,
+  regionLabel,
+  lamportsToSol,
+  latencyScore,
+} from "./utils.js";
